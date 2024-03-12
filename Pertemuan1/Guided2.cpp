d62b840fc2a0fd7fb1aa644d0003981172a6e41a
NaufaliLutS <naufalluthfi1297@gmail.com>
2024-03-12 14:18:41 +0700

    cmmit modul 1

--- Pertemuan1/Guided2.cpp
+++ Pertemuan1/Guided2.cpp
@@ -3,30 +3,30 @@
 
 struct Mahasiswa
 {
-    char name[50];
-    char address[100];
-    int age;
+    char name_125[50];
+    char address_125[100];
+    int age_125;
 };
 int main()
 {
     struct Mahasiswa mhs1, mhs2;
 
-    strcpy(mhs1.name, "Dian");
-    strcpy(mhs1.address, "Mataram");
-    mhs1.age = 22;
-    strcpy(mhs2.name, "Bambang");
-    strcpy(mhs2.address, "Surabaya");
-    mhs2.age = 23;
+    strcpy(mhs1.name_125, "Dian");
+    strcpy(mhs1.address_125, "Mataram");
+    mhs1.age_125 = 22;
+    strcpy(mhs2.name_125, "Bambang");
+    strcpy(mhs2.address_125, "Surabaya");
+    mhs2.age_125 = 23;
 
     printf("## Mahasiswa 1 ##\n");
-    printf("Nama: %s\n", mhs1.name);
-    printf("Alamat: %s\n", mhs1.address);
-    printf("Umur: %d\n", mhs1.age);
+    printf("Nama: %s\n", mhs1.name_125);
+    printf("Alamat: %s\n", mhs1.address_125);
+    printf("Umur: %d\n", mhs1.age_125);
     printf ("\n");
     printf("## Mahasiswa 2 ##\n");
-    printf("Nama: %s\n", mhs2.name);
-    printf("Alamat: %s\n", mhs2.address);
-    printf("Umur: %d\n", mhs2.age);
+    printf("Nama: %s\n", mhs2.name_125);
+    printf("Alamat: %s\n", mhs2.address_125);
+    printf("Umur: %d\n", mhs2.age_125);
     return 0;
 
 }
--- Pertemuan1/Guided3.cpp
+++ Pertemuan1/Guided3.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 
 int main(){
-    int nilai[5];
-    nilai[0] = 23;
-    nilai[1] = 50;
-    nilai[2] = 34;
-    nilai[3] = 78;  
-    nilai[4] = 90;
+    int nilai_125[5];
+    nilai_125[0] = 23;
+    nilai_125[1] = 50;
+    nilai_125[2] = 34;
+    nilai_125[3] = 78;  
+    nilai_125[4] = 90;
 
-    cout << "Isi array pertama : " << nilai[0] << endl;
-    cout << "Isi array kedua : " << nilai[1] << endl;
-    cout << "Isi array ketiga : " << nilai[2] << endl;
-    cout << "Isi array keempat : " << nilai[3] << endl;
-    cout << "Isi array kelima : " << nilai[4] << endl;
+    cout << "Isi array pertama : " << nilai_125[0] << endl;
+    cout << "Isi array kedua : " << nilai_125[1] << endl;
+    cout << "Isi array ketiga : " << nilai_125[2] << endl;
+    cout << "Isi array keempat : " << nilai_125[3] << endl;
+    cout << "Isi array kelima : " << nilai_125[4] << endl;
     
     return 0;
 }
