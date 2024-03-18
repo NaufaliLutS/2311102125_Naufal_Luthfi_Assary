e226ff816e5a7fb2e586263134c41314e13e4171
NaufaliLutS <naufalluthfi1297@gmail.com>
2024-03-19 03:33:44 +0700

    commit modul 2

--- Pertemuan 2-Array/Laprak_modul2.md
+++ Pertemuan 2-Array/Laprak_modul2.md
@@ -184,10 +184,68 @@ Program diatas ialah program yang menggunakan Array yang digunakan memasukkan uk
 
 
 ```C++
+// Laprak 2 - Unguided 1
+// Naufal Luthfi Assary - 2311102125
+// IF-11-D
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int X_125, Y_125, Z_125; // Deklarasi Variabel Untuk Ukuran Array 3 Dimensi
+
+    // Meminta Input Ukuran Array untuk Setiap Dimensi
+    cout << "------------------------------------------\n";
+    cout << "|---> Naufal-Program Array 3 Dimensi <---|\n";
+    cout << "------------------------------------------\n";
+    cout << "Masukkan Ukuran Array Dimensi X : "; cin >> X_125;
+    cout << "Masukkan Ukuran Array Dimensi Y : "; cin >> Y_125;
+    cout << "Masukkan Ukuran Array Dimensi Z : "; cin >> Z_125;
+
+    // Deklarasi Array 3 Dimensi dengan Ukuran yang dimasukkan Oleh User
+    int Arr_125[X_125][Y_125][Z_125];
+
+    // Meminta User Untuk Memasukkan Elemen Array
+    cout << "Masukkan Elemen Array:" << endl;
+    for (int x = 0; x < X_125; x++) {
+        for (int y = 0; y < Y_125; y++) {
+            for (int z = 0; z < Z_125; z++) {
+                cout << "Input Array[" << x << "][" << y << "][" << z << "]: ";
+                cin >> Arr_125[x][y][z];
+            }
+        }
+        cout << endl;
+    }
+
+    // Menampilkan Sebagian Data Array 3 Dimensi
+    for (int x = 0; x < X_125; x++) {
+        for (int y = 0; y < Y_125; y++) {
+            for (int z = 0; z < Z_125; z++) {
+                cout << "Data Array[" << x << "][" << y << "][" << z << "] = " << Arr_125[x][y][z] << endl;
+            }
+        }
+        cout << endl;
+    }
+
+    // Menampilkan Seluruh Data Array 3 Dimensi
+    cout << "Hasil Array 3 Dimensi:" << endl;
+    for (int x = 0; x < X_125; x++) {
+        for (int y = 0; y < Y_125; y++) {
+            for (int z = 0; z < Z_125; z++) {
+                cout << Arr_125[x][y][z] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
 
 ```
 #### Output:
-![240302_00h00m06s_screenshot](https://github.com/NaufaliLutS/2311102125_Naufal_Luthfi_Assary/blob/main/Pertemuan1/Screenshot%202024-03-12%20222555.png)
+![Output](Naufal-Output_Unguided2_1.png)
+![Output](Naufal-Output_Unguided2_2.png)
 
 Deskripsi :
 Program diatas ialah program untuk menyimpan data Novel menggunakan Class yang dimana menginputkannya menggunakan strcpy dan dipanggil menggunakan cout.
--- Pertemuan 2-Array/Unguided2.cpp
+++ Pertemuan 2-Array/Unguided2.cpp
@@ -1,30 +1,52 @@
+// Laprak 2 - Unguided 1
+// Naufal Luthfi Assary - 2311102125
+// IF-11-D
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    int xSize, ySize, zSize;
+    int X_125, Y_125, Z_125; // Deklarasi Variabel Untuk Ukuran Array 3 Dimensi
 
-    cout << "Masukkan ukuran array tiga dimensi (x y z): ";
-    cin >> xSize >> ySize >> zSize;
+    // Meminta Input Ukuran Array untuk Setiap Dimensi
+    cout << "------------------------------------------\n";
+    cout << "|---> Naufal-Program Array 3 Dimensi <---|\n";
+    cout << "------------------------------------------\n";
+    cout << "Masukkan Ukuran Array Dimensi X : "; cin >> X_125;
+    cout << "Masukkan Ukuran Array Dimensi Y : "; cin >> Y_125;
+    cout << "Masukkan Ukuran Array Dimensi Z : "; cin >> Z_125;
 
-    int array3D[xSize][ySize][zSize];
+    // Deklarasi Array 3 Dimensi dengan Ukuran yang dimasukkan Oleh User
+    int Arr_125[X_125][Y_125][Z_125];
 
-    cout << "Masukkan elemen-elemen array:" << endl;
-    for (int x = 0; x < xSize; ++x) {
-        for (int y = 0; y < ySize; ++y) {
-            for (int z = 0; z < zSize; ++z) {
-                cout << "array[" << x << "][" << y << "][" << z << "]: ";
-                cin >> array3D[x][y][z];
+    // Meminta User Untuk Memasukkan Elemen Array
+    cout << "Masukkan Elemen Array:" << endl;
+    for (int x = 0; x < X_125; x++) {
+        for (int y = 0; y < Y_125; y++) {
+            for (int z = 0; z < Z_125; z++) {
+                cout << "Input Array[" << x << "][" << y << "][" << z << "]: ";
+                cin >> Arr_125[x][y][z];
             }
         }
+        cout << endl;
     }
 
-    cout << "Array yang dimasukkan:" << endl;
-    for (int x = 0; x < xSize; ++x) {
-        for (int y = 0; y < ySize; ++y) {
-            for (int z = 0; z < zSize; ++z) {
-                cout << array3D[x][y][z] << " ";
+    // Menampilkan Sebagian Data Array 3 Dimensi
+    for (int x = 0; x < X_125; x++) {
+        for (int y = 0; y < Y_125; y++) {
+            for (int z = 0; z < Z_125; z++) {
+                cout << "Data Array[" << x << "][" << y << "][" << z << "] = " << Arr_125[x][y][z] << endl;
+            }
+        }
+        cout << endl;
+    }
+
+    // Menampilkan Seluruh Data Array 3 Dimensi
+    cout << "Hasil Array 3 Dimensi:" << endl;
+    for (int x = 0; x < X_125; x++) {
+        for (int y = 0; y < Y_125; y++) {
+            for (int z = 0; z < Z_125; z++) {
+                cout << Arr_125[x][y][z] << " ";
             }
             cout << endl;
         }
